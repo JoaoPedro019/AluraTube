e894c8bfdaa962128a8c3e62110bc0b100ef177f
João Pedro Pereira da Silva <110321358+JoaoPedro019@users.noreply.github.com>
2022-11-11 01:49:26 -0300

    Update CSSreset.js

--- src/components/CSSreset.js
+++ src/components/CSSreset.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export const CSSReset = createGlobalStyle`
+export const CSSreset = createGlobalStyle`
   /* Reset */
   * {
     margin: 0;
